2f4bfd3e1d6c9701f7101b446df4dad8ab9c2a9c
migueld2611 <1001.31099503.ucla@gmail.com>
2025-03-21 14:43:08 -0400

    nuevo

--- js/principal.js
+++ js/principal.js
@@ -3,7 +3,7 @@ import Cl_Hija from "./Cl_Hija.js";
 
 
 let instanciaHija1 = new Cl_Hija(1,2);
-
+let instanciaHija2 = new Cl_Hija(1,2);
 console.log(`valor: ${instanciaHija1.atribaA}`)
-console.log(`valor: ${instanciaHija1.atribaB}`)
+console.log(`valor: ${instanciaHija2.atribaB}`)
 
